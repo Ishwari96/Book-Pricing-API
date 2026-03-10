ffaa7bed6dcb6b4854ca7eed66c00ff67509b7aa
ishwari <ishwaripdoshi@gmail.com>
2026-03-10 11:14:00 +0100

    added more unit tests and changed initial book pricing value

--- src/test/java/com/id/bookshop/service/BookPricingServiceTest.java
+++ src/test/java/com/id/bookshop/service/BookPricingServiceTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 public class BookPricingServiceTest {
 
-    private final BookPricingServiceImpl bookingPricingService = new BookPricingServiceImpl(40);
+    private final BookPricingServiceImpl bookingPricingService = new BookPricingServiceImpl(50);
 
 
     @Test
@@ -38,6 +38,93 @@ public class BookPricingServiceTest {
 
         double price = bookingPricingService.calculatePrice(basket);
 
-        assertEquals(40.0, price, "A basket with 1 book should cost 40 EUR");
+        assertEquals(50.0, price, "A basket with 1 book should cost 50 EUR");
     }
+
+    @Test
+    public void testBasketReturnsPriceForSimilarBooks() {
+        Map<String, Integer> basket = new HashMap<>();
+
+        basket.put("Clean Code", 2);
+
+        double price = bookingPricingService.calculatePrice(basket);
+
+        assertEquals(100.0, price, "A basket with 2 similar books should cost 100 EUR");
+
+    }
+
+    @Test
+    public void testBasketReturnsPriceFor2DifferentBooks() {
+        Map<String, Integer> basket = new HashMap<>();
+
+        basket.put("Clean Code", 1);
+        basket.put("The Clean Coder", 1);
+
+        double price = bookingPricingService.calculatePrice(basket);
+
+        assertEquals(95.0, price, "A basket with 2 different books should cost 95 EUR");
+
+    }
+
+    @Test
+    public void testBasketReturnsPriceFor3DifferentBooks() {
+        Map<String, Integer> basket = new HashMap<>();
+
+        basket.put("Clean Code", 1);
+        basket.put("The Clean Coder", 1);
+        basket.put("The Clean Architecture", 1);
+
+        double price = bookingPricingService.calculatePrice(basket);
+
+        assertEquals(135.0, price, "A basket with 3 different books should cost 135 EUR");
+
+    }
+
+    @Test
+    public void testBasketReturnsPriceFor4DifferentBooks() {
+        Map<String, Integer> basket = new HashMap<>();
+
+        basket.put("Clean Code", 1);
+        basket.put("The Clean Coder", 1);
+        basket.put("The Clean Architecture", 1);
+        basket.put("Test Driven Development", 1);
+
+        double price = bookingPricingService.calculatePrice(basket);
+
+        assertEquals(160.0, price, "A basket with 4 different books should cost 160 EUR");
+
+    }
+
+    @Test
+    public void testBasketReturnsPriceFor5DifferentBooks() {
+        Map<String, Integer> basket = new HashMap<>();
+
+        basket.put("Clean Code", 1);
+        basket.put("The Clean Coder", 1);
+        basket.put("The Clean Architecture", 1);
+        basket.put("Test Driven Development", 1);
+        basket.put("Working with legacy code", 1);
+
+        double price = bookingPricingService.calculatePrice(basket);
+
+        assertEquals(187.5, price, "A basket with 5 different books should cost 187,50 EUR");
+
+    }
+
+    @Test
+    public void testFullSetAndOneBasketReturnsPrice() {
+        Map<String, Integer> basket = new HashMap<>();
+
+        basket.put("Clean Code", 2);
+        basket.put("The Clean Coder", 1);
+        basket.put("The Clean Architecture", 1);
+        basket.put("Test Driven Development", 1);
+        basket.put("Working with legacy code", 1);
+
+        double price = bookingPricingService.calculatePrice(basket);
+
+        assertEquals(237.5, price, "A basket with 5 different books and one similar book should cost 237,50 EUR");
+
+    }
+
 }
